3dc37a89ff238c8edcf561bf96e7d760d35d14ba
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-12 05:23:33 +0530

    Update config.py
    Signed-off-by: tinaarobot <146160310+tinaarobot@users.noreply.github.com>

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/tinaarobot/AnonXMusic",
+    "https://github.com/tinaarobot/ABCMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
